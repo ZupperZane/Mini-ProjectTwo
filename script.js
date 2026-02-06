e4a94c2a8f4d8041fcf5d0a0331900b64ea8e592
ZupperZane <72839201+ZupperZane@users.noreply.github.com>
2026-02-06 14:01:57 -0500

    First Addition for .js
    Arrays need to be finished, Cart Functions need to be finished,Cart needs to be added

---
+++ script.js
@@ -0,0 +1,15 @@
+const CartObjects()
+{
+ NameArray = new Array[("Croissant"),("Eclair"),("LemonBar"),("Muffin")]
+ ImageArray = new Array[()]
+}
+
+function AddToCart(clicked_id) {
+}
+function ClearCart() {
+
+}
+function RemoveFromCart() {
+  const inputElement = document.getElementById("numberEntry");
+  const outputElement = document.getElementById("output");
+}
