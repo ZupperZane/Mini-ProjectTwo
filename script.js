ec24329fa622ff6f35efed41b0a9bdbf1987c26c
HAJ02 <Haj020206@gmail.com>
2026-02-06 15:44:12 -0500

    Clear cart Functional Remove Cart Not entering Logic

--- script.js
+++ script.js
@@ -5,28 +5,35 @@
 
 function AddToCart(clicked_id) {
     CartArray.push(clicked_id)
-      console.log("added to cart");
+      console.log(CartArray);
+
 }
 function ClearCart() {
-    CartArray = new Array[("")]
+    CartArray = new Array("")
         console.log("Cleared cart");
+              console.log(CartArray)
 }
 function RemoveFromCart(index) {
+     console.log("Removing");
  CartArray.splice(index,1);
+ console.log(CartArray);
 }
 function DisplayCartItems(){
     console.log("Accesing Cart")
     const divContainer = document.getElementById("CartItems");
     for (var i=0; i <CartArray.length;i++){
+        console.log(CartArray)
             const p = document.createElement("p");
             const bakedNumber= CartArray[i]
             p.textContent = NameArray[bakedNumber];
-            
             const removeButton = document.createElement("Button");
             removeButton.textContent = "Remove";
-            removeButton.onClick = RemoveFromCart(i)
+           removeButton.onClick  = () => RemoveFromCart(i);
              divContainer.appendChild(p);
              divContainer.appendChild(removeButton);
-            
     }
+    const ClearButton = document.createElement("Button");
+    ClearButton.textContent = "Clear Cart";
+    ClearButton.onclick = () => ClearCart();
+    divContainer.appendChild(ClearButton);
 }
